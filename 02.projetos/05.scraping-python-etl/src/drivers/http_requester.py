59a38a474d27f49c01408d4b96c195b9402bf6e9
juniorsilvacc <juniorsilvacc@hotmail.com>
2026-01-28 01:06:19 -0300

    implementando a classe HttpCollector e http_request_mock - projeto scraping python etl

--- 02.projetos/05.scraping-python-etl/src/drivers/http_requester.py
+++ 02.projetos/05.scraping-python-etl/src/drivers/http_requester.py
@@ -5,7 +5,7 @@ class HttpRequester:
     def  __init__(self) -> None:
         self.__url = "https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm"
         
-    def request_from_page(self) -> Dict[int, str]:
+    def request_from_page(self) -> Dict[int, str | int]:
         response = requests.get(self.__url)
         
         return {
