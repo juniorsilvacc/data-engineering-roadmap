c3b6408ade40a1b86ae59db4eb79387759683dda
juniorsilvacc <juniorsilvacc@hotmail.com>
2026-01-27 21:06:57 -0300

    implementando classe HttpRequest - projeto scraping python etl

--- 02.projetos/05.scraping-python-etl/src/drivers/http_requester.py
+++ 02.projetos/05.scraping-python-etl/src/drivers/http_requester.py
@@ -1,10 +1,14 @@
+from typing import Dict
 import requests
 
 class HttpRequester:
     def  __init__(self) -> None:
         self.__url = "https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm"
         
-    def request_from_page(self):
+    def request_from_page(self) -> Dict[int, str]:
         response = requests.get(self.__url)
-        print(response.status_code)
-        print(response.text)
+        
+        return {
+            "status_code": response.status_code,
+            "html": response.text
+        }
